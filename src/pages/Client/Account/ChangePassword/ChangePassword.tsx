bdda7a47aeae3cd633d291f700a9ef35f1caeb86
João Vitor S. Rabelo <joao_vitor.personal@outlook.com>
2022-08-02 09:01:15 -0300

    feat: add change password and close account

---
+++ src/pages/Client/Account/ChangePassword/ChangePassword.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+// style
+import './ChangePassword.css'
+import { BsFillExclamationSquareFill } from 'react-icons/bs'
+import { AiFillLock, AiFillUnlock } from 'react-icons/ai'
+
+interface IProps {}
+
+const ChangePassword = (props: IProps) => {
+  return (
+    <section className="changePassword">
+      <div className="warning">
+        <BsFillExclamationSquareFill className="icons" />
+        <span>You should enter your current password to change:</span>
+      </div>
+      <form>
+        <label>
+          <span>Password:</span>
+          <AiFillUnlock className="icons" />
+          <input type="password" placeholder="Current Password" />
+        </label>
+        <label>
+          <span>New Password:</span>
+          <AiFillLock className="icons" />
+          <input type="password" placeholder="New Password" />
+        </label>
+        <label>
+          <span>New Password Confirmation:</span>
+          <AiFillLock className="icons" />
+          <input type="password" placeholder="New Password Confirmation" />
+        </label>
+        <button className="btn btn-primary">Salvar</button>
+      </form>
+    </section>
+  )
+}
+
+export default ChangePassword
